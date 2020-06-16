8c8c5be42bce439c33171b11b443d8dfa3375243
Thomas Marchand <thomas.marchand@tuta.io>
2020-06-16 15:53:59 +0200

    load config

--- examples/get_started.py
+++ examples/get_started.py
@@ -1,7 +1,32 @@
+import sys, toml, unittest
+
 # to import binance.py from the dev directory
-import sys, unittest
 sys.path.append("../")
 
 import binance
 
-client =  binance.Client()
+
+class Config:
+    def __init__(self, file_name, template_name):
+        config_file = self.extract_config(file_name, template_name)
+        self.load_config(config_file)
+
+    def get_path(self, name):
+        return os.path.join(os.path.dirname(os.path.realpath(__file__)), name)
+
+    def extract_config(self, file_name, template_name):
+        config_file = self.get_path(file_name)
+        if not os.path.isfile(config_file):
+            print("config file doesn't exist, copying template!")
+            shutil.copyfile(self.get_path(template_name), config_file)
+        return config_file
+
+    def load_config(self, config_file):
+        config = toml.load(config_file)
+
+        binance = config["binance"]
+        self.api_key = binance["api_key"]
+        self.api_secret = binance["api_secret"]
+
+client = binance.Client()
+

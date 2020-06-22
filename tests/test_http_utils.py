bca4be072cf1f4e5a1d89b6c19cde46b2c8571fc
Thomas Marchand <thomas.marchand@tuta.io>
2020-06-23 00:43:54 +0200

    update test

--- tests/test_http_utils.py
+++ tests/test_http_utils.py
@@ -12,7 +12,8 @@ class TestSignature(unittest.TestCase):
             "NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j",
             None,
         )
-        example_query = {
+        """ Original query:
+        {
             "symbol": "LTCBTC",
             "side": "BUY",
             "type": "LIMIT",
@@ -22,6 +23,8 @@ class TestSignature(unittest.TestCase):
             "recvWindow": 5000,
             "timestamp": 1499827319559,
         }
+        """
+        example_query = "symbol=LTCBTC&side=BUY&type=LIMIT&timeInForce=GTC&quantity=1&price=0.1&recvWindow=5000&timestamp=1499827319559"
         self.assertEqual(
             client._generate_signature(example_query),
             "c8db56825ae71d6d79447849e617115f4a920fa2acdcab2b053c4b2838bd6b71",

8e5e355bc940e75e38561610866f181057fcb5a0
Thomas Marchand <thomas.marchand@tuta.io>
2020-06-26 09:25:50 +0200

    remove useless import

--- binance/client.py
+++ binance/client.py
@@ -1,4 +1,3 @@
-import time
 from .http import HttpClient, BinancePyError
 from .web_sockets import UserDataStream
 from . import OrderType
@@ -579,15 +578,15 @@ class Client:
     # https://github.com/binance-exchange/binance-official-api-docs/blob/master/user-data-stream.md#close-a-listenkey
     async def keep_alive_listen_key(self, listen_key):
         if not listen_key:
-            raise ValueError(
-                "This query requires a listen_key."
-            )
-        return await self.http.send_api_call("/api/v3/userDataStream", "PUT", params={"listenKey" : listen_key})
+            raise ValueError("This query requires a listen_key.")
+        return await self.http.send_api_call(
+            "/api/v3/userDataStream", "PUT", params={"listenKey": listen_key}
+        )
 
     # https://github.com/binance-exchange/binance-official-api-docs/blob/master/user-data-stream.md#close-a-listenkey
     async def close_listen_key(self, listen_key):
         if not listen_key:
-            raise ValueError(
-                "This query requires a listen_key."
-            )
-        return await self.http.send_api_call("/api/v3/userDataStream", "DELETE", params={"listenKey" : listen_key})
+            raise ValueError("This query requires a listen_key.")
+        return await self.http.send_api_call(
+            "/api/v3/userDataStream", "DELETE", params={"listenKey": listen_key}
+        )

d7eec4263f26e2732e5c842dea27ece81d8cb6ac
Thomas Marchand <thomas.marchand@tuta.io>
2020-06-27 20:14:51 +0200

    update tests

--- tests/test_http_utils.py
+++ tests/test_http_utils.py
@@ -11,6 +11,7 @@ class TestSignature(unittest.TestCase):
             "vmPUZE6mv9SD5VNHk4HlWFsOr6aKE2zvsw0MuIgwCIPy6utIco14y7Ju91duEh8A",
             "NhqPtmdSJYdKjVHjA7PZj4Mge3R5YNiP1e3UZjInClVN65XAbvqqM6A7H5fATj0j",
             None,
+            None,
         )
         """ Original query:
         {

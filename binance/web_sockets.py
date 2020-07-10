02694db8ce5d836a0bc49e1509ec7b99ce0e1c8d
Thomas Marchand <thomas.marchand@tuta.io>
2020-07-10 22:57:03 +0200

    improve session using with

--- binance/web_sockets.py
+++ binance/web_sockets.py
@@ -23,22 +23,22 @@ class UserDataStream:
             await self.client.keep_alive_listen_key(listen_key)
 
     async def connect(self):
-        session = aiohttp.ClientSession()
-        listen_key = (await self.client.create_listen_key())["listenKey"]
-        web_socket = await session.ws_connect(f"{self.endpoint}/ws/{listen_key}")
-        asyncio.ensure_future(self._heartbeat(listen_key))
+        async with aiohttp.ClientSession() as session:
+            listen_key = (await self.client.create_listen_key())["listenKey"]
+            web_socket = await session.ws_connect(f"{self.endpoint}/ws/{listen_key}")
+            asyncio.ensure_future(self._heartbeat(listen_key))
 
-        while True:
-            msg = await web_socket.receive()
-            if msg.type in (aiohttp.WSMsgType.CLOSED, aiohttp.WSMsgType.CLOSE):
-                logging.error(
-                    "Trying to receive something while the websocket is closed! Trying to reconnect."
-                )
-                await self.connect()
-            elif msg.type is aiohttp.WSMsgType.ERROR:
-                logging.error(
-                    f"Something went wrong with the websocket, reconnecting..."
-                )
-                await self.connect()
-            event = self.client.events.wrap_event(json.loads(msg.data))
-            event.fire()
+            while True:
+                msg = await web_socket.receive()
+                if msg.type in (aiohttp.WSMsgType.CLOSED, aiohttp.WSMsgType.CLOSE):
+                    logging.error(
+                        "Trying to receive something while the websocket is closed! Trying to reconnect."
+                    )
+                    await self.connect()
+                elif msg.type is aiohttp.WSMsgType.ERROR:
+                    logging.error(
+                        f"Something went wrong with the websocket, reconnecting..."
+                    )
+                    await self.connect()
+                event = self.client.events.wrap_event(json.loads(msg.data))
+                event.fire()

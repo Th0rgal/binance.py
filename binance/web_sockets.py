fb21dc113e1ea56809c51446ac7959ad31ee0796
Thomas Marchand <thomas.marchand@tuta.io>
2020-07-11 17:53:06 +0200

    switch to ensure future in order to avoid recursive limit

--- binance/web_sockets.py
+++ binance/web_sockets.py
@@ -21,12 +21,12 @@ class EventsDataStream:
                 logging.error(
                     "Trying to receive something while the websocket is closed! Trying to reconnect."
                 )
-                await self.connect()
+                asyncio.ensure_future(self.connect())
             elif msg.type is aiohttp.WSMsgType.ERROR:
                 logging.error(
                     f"Something went wrong with the websocket, reconnecting..."
                 )
-                await self.connect()
+                asyncio.ensure_future(self.connect())
             event = self.client.events.wrap_event(json.loads(msg.data))
             event.fire()
 

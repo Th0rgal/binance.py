a654c09f34ad9278dd92521fe4a836b1e350c104
Thomas Marchand <thomas.marchand@tuta.io>
2020-07-12 11:56:24 +0200

    move errors to errors.py

---
+++ binance/errors.py
@@ -0,0 +1,34 @@
+class BinancePyError(Exception):
+    pass
+
+
+class BinanceError(BinancePyError):
+    pass
+
+
+class QueryCanceled(BinancePyError):
+    pass
+
+
+class HTTPError(BinancePyError):
+
+    code = 400
+    message = "Malformed request."
+
+
+class WAFLimitViolated(HTTPError):
+
+    code = 403
+    message = "The WAF Limit (Web Application Firewall) has been violated."
+
+
+class RateLimitReached(HTTPError):
+
+    code = 429
+    message = "The rate limit has been reached."
+
+
+class IPAdressBanned(HTTPError):
+
+    code = 418
+    message = "Your IP address has been auto-banned for continuing to send requests after receiving 429 codes."
